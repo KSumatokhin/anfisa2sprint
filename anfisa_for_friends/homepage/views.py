7a524ac5d04e7846551fce1976f5a0f1f6296821
Konstantin Sumatokhin <ksumatokhin@gmail.com>
2024-04-18 17:06:29 +0200

    update index func

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -5,7 +5,10 @@ from ice_cream.models import IceCream
 
 def index(request):
     template_name = 'homepage/index.html'
-    ice_cream_list = IceCream.objects.values('id', 'title', 'description')
+    ice_cream_list = IceCream.objects.values(
+        'id', 'title', 'description'
+        # Верни только те объекты, у которых в поле is_on_main указано True:
+        ).filter(is_on_main=True)
     # Cписок словарей: <QuerySet [{'id': 1, 'title': 'Золотое мороженое'}, ...]>
     context = {
         'ice_cream_list': ice_cream_list,
